a19425734b7bd75597dd62b332c596e5b36ee7c6
smshaown <smshaown65@gmail.com>
2023-11-19 03:52:45 +0600

    About right mouse slider start

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,6 +15,29 @@ const About = () => {
                 </div>
              </div>
             </div>
+
+            {/* About section start  */}
+            <div>
+                <div className="w-[1080px] m-auto">
+                    <div>
+                        <div className="w-1/2">
+                            <h2 className="text-[40px] font-bold colorBlack"> Everyone's invited </h2>
+                            <div className="mt-[30px]">
+                                <h2 className="text[#101257] font-bold text-[30px]">Sapno ka gateway</h2>
+                                <p className="colorBlack font-bold text-[18px]">Connection Dreamers to the city Of Dreams</p>
+                            </div>
+                            <div className="mt-[30px]">
+                                <h2 className="text[#101257] font-bold text-[30px]">Sustainability</h2>
+                                <p className="colorBlack font-bold text-[18px]">From waste segrgation to green power supply Mumbai Festival is putting the earth first.</p>
+                            </div>
+                            <button className="btn btn-primari mt-8">Knkow more</button>
+                        </div>
+                        <div className="w-1/2">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
